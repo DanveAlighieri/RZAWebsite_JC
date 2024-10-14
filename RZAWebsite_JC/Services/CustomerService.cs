c3441300fab6cfffcf7325a377399635dbfca6f0
S2300716 <S2300716@NDC03198>
2024-10-14 12:18:36 +0100

    Add project files.

---
+++ RZAWebsite_JC/Services/CustomerService.cs
@@ -0,0 +1,14 @@
+﻿using RZAWebsite_JC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RZAWebsite_JC.Services
+{
+    public class CustomerService
+    {
+        private readonly TlS2300716RzaContext _context;
+        public CustomerService(TlS2300716RzaContext context)
+        {
+            _context = context;
+        }
+    }
+}
